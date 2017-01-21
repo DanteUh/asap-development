badb63b8b1f0205c6511d485c27fb8a27a623ad9
attilac <attila.cederbygd@gmail.com>
2017-01-21 23:36:15 +0100

    mobile-nav redux

--- js/nav.js
+++ js/nav.js
@@ -2,10 +2,14 @@
 
 //glob vars
 var hamburger = document.getElementById("hamburger");
+var openIcon = document.getElementById("openIcon");
+var closeIcon = document.getElementById("closeIcon");
+var navWrapper = document.getElementById("nav-bg-wrapper");
 
 //event listeners
 hamburger.addEventListener("click", navDropDown);
 window.addEventListener("scroll", navbarBg);
+
 
 
 //menu drop when hamburger menu is clicked
@@ -13,22 +17,33 @@ function navDropDown(event) {
     event.preventDefault();
     var menu = document.getElementsByClassName("nav-menu")[0];
 
-
     if (menu.id === "menu-up") {
         menu.setAttribute("id", "menu-down");
+        navWrapper.className = "bg-blue";
+        closeIcon.className = "shown";
+        openIcon.className = "hidden";
+
 
     } else if (menu.id === "menu-down") {
         menu.setAttribute("id", "menu-up");
+        if (document.body.scrollTop < (60)) {
+            navWrapper.className = "bg-transparent";
+            openIcon.className = "shown";
+            closeIcon.className = "hidden";
+        }
     }
 }
 
 //navbar background change
 function navbarBg() {
+    var menu = document.getElementsByClassName("nav-menu")[0];
     var viewPort = document.getElementsByTagName("header")[0];
 
-    if (document.body.scrollTop > (60) || document.documentElement.scrollTop > (60)) {
-        document.getElementById("nav-bg-wrapper").className = "bg-blue";
-    } else {
-        document.getElementById("nav-bg-wrapper").className = "";
+    if(menu.id !== "menu-down") {
+        if (document.body.scrollTop > (60) || document.documentElement.scrollTop > (60)) {
+            navWrapper.className = "bg-blue";
+        } else {
+            navWrapper.className = "bg-transparent";
+        }
     }
 }
