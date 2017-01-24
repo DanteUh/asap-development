bec992b149bca84e8684e0fed8b086d00ba45e07
jesperengstrom <jengstro@gmail.com>
2017-01-24 11:02:18 +0100

    experimental hamburger animation on vadvigör. Merga inte!

--- js/nav.js
+++ js/nav.js
@@ -12,29 +12,29 @@ domReady(function() {
     }
 
     var scrollme = document.getElementById("scrollTop");
-    scrollme.addEventListener("click",runScroll,false);
+    scrollme.addEventListener("click", runScroll, false);
 
     /**
      * Scroll to top function
      * 
      */
     function scrollTo(element, to, duration) {
-      if (duration <= 0) return;
-      var difference = to - element.scrollTop;
-      var perTick = difference / duration * 10;
+        if (duration <= 0) return;
+        var difference = to - element.scrollTop;
+        var perTick = difference / duration * 10;
 
-      setTimeout(function() {
-        element.scrollTop = element.scrollTop + perTick;
-        if (element.scrollTop == to) return;
-        scrollTo(element, to, duration - 10);
-      }, 10);
+        setTimeout(function() {
+            element.scrollTop = element.scrollTop + perTick;
+            if (element.scrollTop == to) return;
+            scrollTo(element, to, duration - 10);
+        }, 10);
     }
 
     /****************************
-    * Nav functions
-    *
-    *
-    */
+     * Nav functions
+     *
+     *
+     */
 
     //glob vars
     var hamburger = document.getElementById("hamburger");
@@ -49,7 +49,7 @@ domReady(function() {
     /**
      * menu drop when hamburger menu is clicked
      *
-    */    
+     */
     function navDropDown(event) {
         event.preventDefault();
         var menu = document.getElementsByClassName("nav-menu")[0];
@@ -57,17 +57,19 @@ domReady(function() {
         if (menu.id === "menu-up") {
             menu.setAttribute("id", "menu-down");
             navWrapper.className = "bg-blue";
-            closeIcon.className = "shown";
-            openIcon.className = "hidden";
+            //closeIcon.className = "shown";
+            //openIcon.className = "hidden";
+            hamburger.setAttribute("class", "nav-collapse flipped");
         } else if (menu.id === "menu-down") {
             menu.setAttribute("id", "menu-up");
-            if ( document.body.scrollTop > 60 || window.pageYOffset > 60 || document.documentElement.scrollTop > 60)  {
+            hamburger.setAttribute("class", "nav-collapse");
+            if (document.body.scrollTop > 60 || window.pageYOffset > 60 || document.documentElement.scrollTop > 60) {
                 //
             } else {
                 navWrapper.className = "bg-transparent";
             }
-            openIcon.className = "shown";
-            closeIcon.className = "hidden";
+            //openIcon.className = "shown";
+            //closeIcon.className = "hidden";
         }
     }
 
@@ -79,8 +81,8 @@ domReady(function() {
         var menu = document.getElementsByClassName("nav-menu")[0];
         var viewPort = document.getElementsByTagName("header")[0];
 
-        if(menu.id !== "menu-down") {
-            if ( document.body.scrollTop > 60 || window.pageYOffset > 60 || document.documentElement.scrollTop > 60) {
+        if (menu.id !== "menu-down") {
+            if (document.body.scrollTop > 60 || window.pageYOffset > 60 || document.documentElement.scrollTop > 60) {
                 navWrapper.className = "bg-blue";
             } else {
                 navWrapper.className = "bg-transparent";
